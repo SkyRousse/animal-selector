a23e4887c3945555a8a172006a0a74b7ee28e463
Kevin VanEvery and Sky Rousse <sky@>
2016-06-02 09:05:04 -0700

    create form for selecting animals reveal divs about corresponding animal that is selected using else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 /*This file is for your custom js.  All yours*/
+$(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    debugger;
+    var animal = parseInt($("#animal-selector").val());
+    if (animal === 1) {
+      $("#aardvark").show();
+    }
+    else if (animal === 2) {
+      $("#crocodile").show();
+    }
+    else {
+      $("#racoon").show();
+    };
+  })
+});
